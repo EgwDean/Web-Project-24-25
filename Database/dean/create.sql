804568e90f1c8f362bb46403cf408d896825fd17
Konstantinos Anastasopoulos <101580598+EgwDean@users.noreply.github.com>
2024-11-16 16:11:54 +0200

    Update create.sql

--- Database/dean/create.sql
+++ Database/dean/create.sql
@@ -5,7 +5,6 @@ USE diplomatiki_support;
 
 
 CREATE TABLE professor (
-  username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
   surname VARCHAR(255) NOT NULL,
@@ -19,7 +18,6 @@ CREATE TABLE professor (
 
 
 CREATE TABLE student (
-  username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
   surname VARCHAR(255) NOT NULL,
