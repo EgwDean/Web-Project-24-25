8ee89e3a53c5e45b809ee0d5d64341be52fc9572
Charalampos Anastasiou <39501205+harryanst@users.noreply.github.com>
2024-12-24 23:55:47 +0200

    Update tables_triggers_procedures.sql

--- Project/database/tables_triggers_procedures.sql
+++ Project/database/tables_triggers_procedures.sql
@@ -511,6 +511,32 @@ DELIMITER ;
 
 -- Υπό Εξέταση Διπλωματική
 
+
+
+DELIMITER $
+DROP PROCEDURE IF EXISTS setUnderExam$
+CREATE PROCEDURE setUnderExam(IN dip_id INT,  OUT error_code INT)
+BEGIN 
+DECLARE protocol INT;
+
+SELECT anathesi_diplomatikis.protocol_number INTO protocol FROM anathesi_diplomatikis 
+WHERE id_diploma = dip_id
+LIMIT 1;
+
+IF (protocol IS NOT NULL) THEN
+	UPDATE anathesi_diplomatikis 
+	SET status = 'under examination' 
+	WHERE id_diploma = dip_id AND status = 'active';
+    
+    SET error_code = 0;
+ELSE 
+	SET error_code = 1;
+END IF;
+END$
+DELIMITER ;
+
+
+
 DELIMITER $
 DROP PROCEDURE IF EXISTS gradeSubmit$
 CREATE PROCEDURE gradeSubmit(IN professor_email VARCHAR(255), IN id_diploma INT, IN grade DECIMAL(4, 2))
@@ -775,4 +801,3 @@ BEGIN
 END$
 
 DELIMITER ;
-
