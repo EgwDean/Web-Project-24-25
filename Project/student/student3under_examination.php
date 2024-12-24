3715b4e7250596d95c006cdc0a8ef4b592bc3719
Charalampos Anastasiou <39501205+harryanst@users.noreply.github.com>
2024-12-24 18:01:41 +0200

    Update student3under_examination.php

--- Project/student/student3under_examination.php
+++ Project/student/student3under_examination.php
@@ -34,227 +34,130 @@ if (isset($_SESSION['status'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.3/xlsx.full.min.js"></script>
     <title>Student Page</title>
-    <style>
-        /* Στυλ για τη σελίδα */
-        body {
-            font-family: Calibri, Arial, sans-serif;
-            margin: 0;
-            background-color: #f4f4f4;
-            display: flex;
-            flex-direction: column;
-			height: 100vh
-        }
+	<link rel="stylesheet" type="text/css" href="student3under_examination.css">
+</head>
+<body>
+   <!-- Navigation bar -->
+    <div class="navbar">
+        <div class="logo">
+            <a href="student.php">
+                <img src="/Project/media/logo.png" alt="Logo" class="logo-img" style="height: 40px;">
+            </a>
+        </div>
 
-        .navbar {
-            background-color: #333;
-            color: #fff;
-            padding: 15px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
+        <div class="menu">
+            <div>
+                <a href="student.php" class="menu-item">Προβολή Θέματος</a>
+            </div>
+            <div>
+                <a href="#" class="menu-item" onclick="statusRedirectProccess()">Επεξεργασία Προφίλ</a>
+            </div>
+            <div>
+                <a href="#" class="menu-item" onclick="statusRedirectManagement()">Διαχείριση Διπλωματικής</a>
+            </div>
+        </div>
 
-        .navbar .menu {
-            display: flex;
-            gap: 20px;
-            justify-content: center;
-            flex: 1;
-        }
+        <div class="user-info">
+            <span><?php echo $userEmail; ?></span>
+            <form method="POST" action="">
+                <button type="submit" name="logout" class="logout-btn">Logout</button>
+            </form>
+        </div>
+    </div>
 
-        .menu-item {
-            color: #fff;
-			background-color: #444;
-            text-decoration: none;
-            padding: 10px;
-			transition: background-color 0.3s ease;
-			border-radius: 4px;
-        }
+    <!-- Main content -->
+    <div class="content">
+        <!-- Left column -->
+        <div class="column">
+            <div class="box">
+				<h2>
+					Πληροφορίες Εξέτασης
+				</h2>
+				<table id="item_table1">
+					<!-- Τα δεδομένα εισάγονται εδώ δυναμικά -->
+				</table>
+				<table id="item_table2">
+					<!-- Τα δεδομένα εισάγονται εδώ δυναμικά -->
+				</table>
+            </div>
+			<div class="box">
+    			<h2>Προγραμματισμός Εξέτασης</h2>
+    
+				<!-- Φόρμα για προγραμματισμό εξέτασης -->
+				<form id=examForm>
+					<!-- Ημερομηνία και ώρα -->
+					<label for="dateField" class="form-label">Ημερομηνία και Ώρα</label>
+					<input type="text" class="form-input" id="dateField"  placeholder="YYYY-MM-DD HH:MM:SS" required>
+					<br><br>
+					
+					<!-- Δωμάτιο/link -->
+					<label for="roomField" class="form-label">Αίθουσα/Link</label>
+					<input type="text" class="form-input" id="roomField" placeholder="Φυσική/Ηλεκτρονική Αίθουσα Εξέτασης" required>
+					<br><br>
+					
+					<!-- Κουμπί υποβολής -->
+					<button type="submit" class="form-button" id="submitExam">Υποβολή</button>
+				</form>
+			</div>
+        </div>
 
-		.menu-item:hover {
-			background-color: #00BFFF;
-			color: white;
-		}
+        <!-- Right column -->
+        <div class="column">
+			<div class="box">
+			<h2>Καταχώρηση Αρχείων Διπλωματικής</h2>
+			<h3>Κείμενο Διπλωματικής: <a id="fileLabel" href="#" target="_blank"><!-- Εδώ μπαίνει το όνομα του file δυναμικά --></a></h3>
+			<input type="file" id="inputProxeiro" accept=".pdf" class="hidden"> <!-- Input για pdf file με πρόχειο διπλωματικής -->
+			<button class="form-button" id="addProxeiro">Ανέβασμα Αρχείου</button> 
+			<div id="input-fields-container">
+				<h3>Εξωτερικοί Σύνδεσμοι:</h3>
+				<form id="linksForm"> <!-- Φόρμα για Links -->
+					<div class="input-container">
+						<input type="text" class="form-input" placeholder="σύνδεσμος" required>
+					</div>
+					<button type="button" class="form-button" onclick="addTextField()">Προσθήκη Νέου Συνδέσμου</button>
+					<button type="submit" class="form-button">Καταχώρηση</button>
+				</form>
+			</div>
 
-        .navbar .user-info {
-            display: flex;
-            align-items: center;
-            gap: 15px;
-            white-space: nowrap;
-        }
+			<script>
+				function addTextField() { // Προσθέτει inputs για extra links
+					const newDiv = document.createElement("div");
+					newDiv.classList.add("input-container");
+					const newInput = document.createElement("input");
+					newInput.type = "text";
+					newInput.classList.add("form-input");
+					newInput.required = true;
+					newInput.placeholder = "σύνδεσμος";
+					newDiv.appendChild(newInput);
+					const form = document.getElementById("linksForm");
+					const firstInputContainer = form.querySelector(".input-container");
+					form.insertBefore(newDiv, firstInputContainer.nextSibling);
+				}
+			</script>
+		</div>
+			<div class="box">
+				<h2>Στοιχεία Περατωμένης Διπλωματικής</h2>
+				<h3>Σύνδεσμος προς Νημερτή: <a id="nemertesLabel" href="#" target="_blank"><!-- Εδώ μπαίνει το link δυναμικά --></a></h3>
+				<form id="nemertesForm">
+					<input type="text" id="nemertesInput" class="form-input" placeholder="σύνδεσμος" required>
+					<button type="submit" id="nemertesButton" class="form-button">Ανέβασμα Συνδέσμου</button>
+				</form>
+				<div class="praktiko-container">
+					<button id="praktikoButton" class="form-button" disabled>Δημιουργία Πρακτικού Εξέτασης</button>
+					<button id="showPraktikoButton" class="form-button" disabled>Προβολή Πρακτικού Εξέτασης</button>
+				</div>
+				<label id="praktikoLabel">*Για διπλωματικές με προβιβάσιμο βαθμό</label>
+			</div>
+        </div>
+    </div>
 
-        .logout-btn {
-            padding: 8px 15px;
-            background-color: #444;
-            color: #fff;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-			transition: background-color 0.3s ease;
-        }
-
-        .logout-btn:hover {
-            background-color: #00BFFF;
-        }
-
-        /* Στυλ για τη διάταξη των στηλών */
-		.content {
-			flex: 1; 
-			display: flex;
-			gap: 10px;
-			padding: 10px;
-			box-sizing: border-box;
-			overflow: auto;
-			margin-top: 10px;
-		}
-
-		.column {
-			flex: 1; 
-			display: flex;
-			flex-direction: column; 
-			gap: 10px;
-		}
-
-		.box {
-			flex: 1; 
-			justify-content: center;
-			align-items: center;
-			background-color: #e9e9e9;
-			padding: 20px;
-			border-radius: 10px;
-			box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-			box-sizing: border-box; 
-			text-align: center;
-		}
-
-		footer {
-			background-color: #192f59;
-			color: white;
-			text-align: center;
-			padding: 10px 0;
-			width: 100%;
-		}
-
-		.form-label {
-		font-size: 16px;
-		color: #333;
-		display: block;
-		text-align: left;
-		}
-
-		.form-input {
-		width: 100%;
-		border: 1px solid #ccc;
-		border-radius: 4px;
-		font-size: 14px;
-		}
-
-		.form-button {
-		background-color: #fff;
-		color: black;
-		border: none;
-		border-radius: 4px;
-		cursor: pointer;
-		font-size: 16px;
-		transition: background-color 0.3s ease;
-		transition: color 0.3s ease;
-		margin-top: 5px;
-		}
-
-		.form-button:hover {
-		background-color: #00BFFF;
-		color: white;
-		}
-
-		#praktikoButton {
-			background-color: #fff;
-			color: black;
-			border: none;
-			border-radius: 4px;
-			cursor: pointer;
-			font-size: 16px;
-			transition: background-color 0.3s ease;
-			transition: color 0.3s ease;
-			margin-top: 30px;
-			margin-right: 2px;
-			margin-left: 2px;
-		}
-
-		#showPraktikoButton {
-			background-color: #fff;
-			color: black;
-			border: none;
-			border-radius: 4px;
-			cursor: pointer;
-			font-size: 16px;
-			transition: background-color 0.3s ease;
-			transition: color 0.3s ease;
-			margin-top: 30px;
-			margin-right: 2px;
-			margin-left: 2px;
-		}
-
-		#showPraktikoButton:enabled:hover {
-			background-color: #00BFFF;
-			color: white;
-		}
-
-		#showPraktikoButton:disabled {
-			cursor: not-allowed;
-			background-color: #ccc;
-			color: #666;
-		}
-
-		#praktikoButton:disabled {
-			cursor: not-allowed;
-			background-color: #ccc;
-			color: #666;
-		}
-
-		#praktikoButton:enabled:hover {
-			background-color: #00BFFF;
-			color: white;
-		}
-
-		#praktikoLabel {
-			font-size: 12px;
-			color: #F00;
-			margin-top: 5px;
-			display: block;
-			text-align: center;
-		}
-
-		.hidden {
-			display: none;
-		}
-
-		#fileLabel a {
-			display: inline-block;
-			margin-bottom: 2px;
-			color: white;
-		}
-
-		#nemertesLabel a {
-			display: inline-block;
-			margin-bottom: 2px;
-			color: white; 
-		}
-
-		h3 {
-			display: block;
-			margin: 1px;
-			margin-top: 8px;
-			background-color: #00BFFF;
-			color: white;
-			border: 1px solid #000;
-		}
-
-		.praktiko-container {
-			display: flex;
-			justify-content: center;
-		}
-
-    </style>
-
+	<!-- Footer -->
+	<footer>
+        <p>&copy; 2024 University of Patras - All Rights Reserved</p>
+    </footer>
+	
+	
+	
 <script>	
 
      // Συνάρτηση που ελέγχει το status διπλωματικής και ανακατευθύνει κατάλληλα με το πάτημα του "Διαχείριση Διπλωματικής"
@@ -824,128 +727,6 @@ if (isset($_SESSION['status'])) {
 	}
 
 </script>
-
-
-</head>
-<body>
-   <!-- Navigation bar -->
-    <div class="navbar">
-        <div class="logo">
-            <a href="student.php">
-                <img src="/Project/media/logo.png" alt="Logo" class="logo-img" style="height: 40px;">
-            </a>
-        </div>
-
-        <div class="menu">
-            <div>
-                <a href="student.php" class="menu-item">Προβολή Θέματος</a>
-            </div>
-            <div>
-                <a href="#" class="menu-item" onclick="statusRedirectProccess()">Επεξεργασία Προφίλ</a>
-            </div>
-            <div>
-                <a href="#" class="menu-item" onclick="statusRedirectManagement()">Διαχείριση Διπλωματικής</a>
-            </div>
-        </div>
-
-        <div class="user-info">
-            <span><?php echo $userEmail; ?></span>
-            <form method="POST" action="">
-                <button type="submit" name="logout" class="logout-btn">Logout</button>
-            </form>
-        </div>
-    </div>
-
-    <!-- Main content -->
-    <div class="content">
-        <!-- Left column -->
-        <div class="column">
-            <div class="box">
-				<h2>
-					Πληροφορίες Εξέτασης
-				</h2>
-				<table id="item_table1">
-					<!-- Τα δεδομένα εισάγονται εδώ δυναμικά -->
-				</table>
-				<table id="item_table2">
-					<!-- Τα δεδομένα εισάγονται εδώ δυναμικά -->
-				</table>
-            </div>
-			<div class="box">
-    			<h2>Προγραμματισμός Εξέτασης</h2>
-    
-				<!-- Φόρμα για προγραμματισμό εξέτασης -->
-				<form id=examForm>
-					<!-- Ημερομηνία και ώρα -->
-					<label for="dateField" class="form-label">Ημερομηνία και Ώρα</label>
-					<input type="text" class="form-input" id="dateField"  placeholder="YYYY-MM-DD HH:MM:SS" required>
-					<br><br>
-					
-					<!-- Δωμάτιο/link -->
-					<label for="roomField" class="form-label">Αίθουσα/Link</label>
-					<input type="text" class="form-input" id="roomField" placeholder="Φυσική/Ηλεκτρονική Αίθουσα Εξέτασης" required>
-					<br><br>
-					
-					<!-- Κουμπί υποβολής -->
-					<button type="submit" class="form-button" id="submitExam">Υποβολή</button>
-				</form>
-			</div>
-        </div>
-
-        <!-- Right column -->
-        <div class="column">
-			<div class="box">
-			<h2>Καταχώρηση Αρχείων Διπλωματικής</h2>
-			<h3>Κείμενο Διπλωματικής: <a id="fileLabel" href="#" target="_blank"><!-- Εδώ μπαίνει το όνομα του file δυναμικά --></a></h3>
-			<input type="file" id="inputProxeiro" accept=".pdf" class="hidden"> <!-- Input για pdf file με πρόχειο διπλωματικής -->
-			<button class="form-button" id="addProxeiro">Ανέβασμα Αρχείου</button> 
-			<div id="input-fields-container">
-				<h3>Εξωτερικοί Σύνδεσμοι:</h3>
-				<form id="linksForm"> <!-- Φόρμα για Links -->
-					<div class="input-container">
-						<input type="text" class="form-input" placeholder="σύνδεσμος" required>
-					</div>
-					<button type="button" class="form-button" onclick="addTextField()">Προσθήκη Νέου Συνδέσμου</button>
-					<button type="submit" class="form-button">Καταχώρηση</button>
-				</form>
-			</div>
-
-			<script>
-				function addTextField() { // Προσθέτει inputs για extra links
-					const newDiv = document.createElement("div");
-					newDiv.classList.add("input-container");
-					const newInput = document.createElement("input");
-					newInput.type = "text";
-					newInput.classList.add("form-input");
-					newInput.required = true;
-					newInput.placeholder = "σύνδεσμος";
-					newDiv.appendChild(newInput);
-					const form = document.getElementById("linksForm");
-					const firstInputContainer = form.querySelector(".input-container");
-					form.insertBefore(newDiv, firstInputContainer.nextSibling);
-				}
-			</script>
-		</div>
-			<div class="box">
-				<h2>Στοιχεία Περατωμένης Διπλωματικής</h2>
-				<h3>Σύνδεσμος προς Νημερτή: <a id="nemertesLabel" href="#" target="_blank"><!-- Εδώ μπαίνει το link δυναμικά --></a></h3>
-				<form id="nemertesForm">
-					<input type="text" id="nemertesInput" class="form-input" placeholder="σύνδεσμος" required>
-					<button type="submit" id="nemertesButton" class="form-button">Ανέβασμα Συνδέσμου</button>
-				</form>
-				<div class="praktiko-container">
-					<button id="praktikoButton" class="form-button" disabled>Δημιουργία Πρακτικού Εξέτασης</button>
-					<button id="showPraktikoButton" class="form-button" disabled>Προβολή Πρακτικού Εξέτασης</button>
-				</div>
-				<label id="praktikoLabel">*Για διπλωματικές με προβιβάσιμο βαθμό</label>
-			</div>
-        </div>
-    </div>
-
-	<!-- Footer -->
-	<footer>
-        <p>&copy; 2024 University of Patras - All Rights Reserved</p>
-    </footer>
 
 </body>
 </html>
