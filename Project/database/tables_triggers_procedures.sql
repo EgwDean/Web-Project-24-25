6751580f95f86c1c1742d2410b6fcade5675cea0
Theofrastos Paximadis <161208255+Theofrastos7@users.noreply.github.com>
2025-01-12 20:51:47 +0200

    Update tables_triggers_procedures.sql

--- Project/database/tables_triggers_procedures.sql
+++ Project/database/tables_triggers_procedures.sql
@@ -514,15 +514,20 @@ DROP PROCEDURE IF EXISTS setUnderExam$
 CREATE PROCEDURE setUnderExam(IN dip_id INT,  OUT error_code INT)
 BEGIN 
 DECLARE protocol INT;
+DECLARE email_student VARCHAR(255);
 
-SELECT anathesi_diplomatikis.protocol_number INTO protocol FROM anathesi_diplomatikis 
-WHERE id_diploma = dip_id
-LIMIT 1;
+SELECT anathesi_diplomatikis.protocol_number, email_stud INTO protocol, email_student FROM anathesi_diplomatikis 
+WHERE id_diploma = dip_id AND status = 'active';
+
+SELECT email_student;
 
 IF (protocol IS NOT NULL) THEN
 	UPDATE anathesi_diplomatikis 
 	SET status = 'under examination' 
 	WHERE id_diploma = dip_id AND status = 'active';
+       
+    INSERT INTO eksetasi_diplomatikis(id_diplomatikis, email_st)
+    VALUES (dip_id, email_student);
     
     SET error_code = 0;
 ELSE 
